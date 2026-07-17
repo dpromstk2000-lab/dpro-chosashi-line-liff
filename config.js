e558c616b03f5f2cb636f0266147f57092722377
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-17 11:14:57 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+window.DPRO_CONFIG = Object.freeze({
+  systemName: "DPRO 土地家屋調査士 LINE",
+  version: "CHOSASHI-4-CUSTOMER-INQUIRY-20260717",
+  officeCode: "dpro_chosashi_demo",
+  apiBase: "https://dpro-chosashi-line-api.dpromstk2000.workers.dev",
+  liffId: "",
+  demoLineUserId: "U_DEMO_CHOSASHI_001",
+  timezone: "Asia/Tokyo",
+  defaultSlotMinutes: 30,
+  supportPhone: "092-000-0000"
+});
