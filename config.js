cf4a808dfc5e271a8ff31df192c6562160c9719c
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-17 13:51:41 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DPRO_CONFIG = Object.freeze({
   systemName: "DPRO 土地家屋調査士 LINE",
-  version: "CHOSASHI-7-FIELD-STAFF-IPAD-20260717",
+  version: "CHOSASHI-8-SYSTEM-CHECK-FINAL-20260717",
   officeCode: "dpro_chosashi_demo",
   apiBase: "https://dpro-chosashi-line-api.dpromstk2000.workers.dev",
   liffId: "",
@@ -10,5 +10,6 @@ window.DPRO_CONFIG = Object.freeze({
   supportPhone: "092-000-0000",
   maxUploadBytes: 15728640,
   allowedUploadTypes: ["application/pdf", "image/jpeg", "image/png", "image/webp"],
-  allowedFieldPhotoTypes: ["image/jpeg", "image/png", "image/webp"]
+  allowedFieldPhotoTypes: ["image/jpeg", "image/png", "image/webp"],
+  releaseVersion: "CHOSASHI-8-WORKER-SYSTEM-CHECK-FINAL-20260717"
 });
